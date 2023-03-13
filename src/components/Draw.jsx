f7c70dd951b9c969a7f8b35fdb3982b7fbebab90
puntogris <rosariodota3@gmail.com>
2023-03-13 19:23:05 -0300

    added hero

---
+++ src/components/Draw.jsx
@@ -0,0 +1,11 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+export default function Draw() {
+  return (
+    <>
+      <div className="h-screen">
+        <Excalidraw />
+      </div>
+    </>
+  );
+}
