b136e3db0716b778a9b16c33131928a2d51fde90
puntogris <rosariodota3@gmail.com>
2023-05-28 19:03:40 -0300

    added useful tips to new scene page

--- app/routes/(app)/dashboard/new.tsx
+++ app/routes/(app)/dashboard/new.tsx
@@ -111,72 +111,107 @@ export default function New() {
   }
 
   return (
-    <Form
-      method="post"
-      className="mx-auto mt-14 flex w-full max-w-3xl flex-shrink-0 flex-col rounded p-12 shadow-xl"
-    >
-      <h1 className="text-center text-xl font-bold">Create a new scene</h1>
-      <h2 className="mt-2 text-sm text-slate-600">
-        <span className="font-semibold">A unique name is required </span>
-        as it will be the ID of the scene, as this is intended for personal use
-        all scenes are located at the root,{" "}
-        <span className="font-semibold">
-          all scenes are private by default
-        </span>{" "}
-        and only you can access them.
-      </h2>
-      <h2 className="mt-2 text-center text-sm font-bold text-blue-500">
-        draw.puntogris.com/{name.length == 0 ? "super-cool-id" : name}
-      </h2>
-      <label className="mb-2 mt-3 block self-start text-sm">Name</label>
-      <input
-        type="text"
-        name="name"
-        value={name}
-        onChange={(e) => validateAndSetName(e.target.value)}
-        className="block w-full rounded-md border border-gray-200 px-4 py-3 text-sm outline-none"
-      />
-      <label className="mb-2 mt-3 block self-start text-sm">Description</label>
-      <input
-        name="description"
-        type="text"
-        className="block w-full rounded-md border border-gray-200 px-4 py-3 text-sm outline-none"
-      />
-      <div className="mt-3 flex items-center justify-between gap-2">
-        <div className="flex flex-col">
-          <label className="text-sm">Visibility</label>
-          <label className="text-sm text-slate-700">
-            This will make public this ID, it can be changed later.
+    <div className="flex h-full w-full">
+      <div className="flex w-1/2 flex-col px-16 py-10">
+        <h1 className="text-xl font-bold">Create a new scene</h1>
+        <p className="text-sm text-zinc-600">
+          Here we go, check the usefull tips to learn more how this works.
+        </p>
+        <h1 className="mt-7 font-bold text-slate-700">Scene configuration</h1>
+        <h2 className="mt-1 text-sm text-zinc-600">
+          You will find your new cool scene at{" "}
+          <span className="font-bold text-blue-500">
+            draw.puntogris.com/{name.length == 0 ? "super-cool-id" : name}
+          </span>
+        </h2>
+        <Form method="post">
+          <label className="mb-2 mt-3 block self-start text-sm">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => validateAndSetName(e.target.value)}
+            className="block w-full rounded-md border border-gray-200 px-4 py-3 text-sm outline-none"
+          />
+          <label className="mb-2 mt-3 block self-start text-sm">
+            Description
           </label>
-        </div>
-        <input
-          type="checkbox"
-          name="publish"
-          className="h-7 w-[3.25rem] cursor-pointer appearance-none rounded-full border-2 border-transparent bg-gray-200 ring-1 ring-transparent ring-offset-white transition-colors duration-200 ease-in-out before:inline-block before:h-6 before:w-6 before:translate-x-0 before:transform before:rounded-full before:bg-white before:shadow before:ring-0 before:transition before:duration-200 before:ease-in-out checked:bg-blue-600 checked:bg-none checked:before:translate-x-full checked:before:bg-blue-200 focus:outline-none dark:bg-gray-700 dark:before:bg-gray-400 dark:checked:bg-blue-600 dark:checked:before:bg-blue-200 dark:focus:ring-offset-gray-800"
-        />
-      </div>
+          <input
+            name="description"
+            type="text"
+            className="block w-full rounded-md border border-gray-200 px-4 py-3 text-sm outline-none"
+          />
+          <div className="mt-3 flex items-center justify-between gap-2">
+            <div className="flex flex-col">
+              <label className="text-sm">Visibility</label>
+              <label className="text-sm text-slate-700">
+                This will make public this ID, it can be changed later.
+              </label>
+            </div>
+            <input
+              type="checkbox"
+              name="publish"
+              className="h-7 w-[3.25rem] cursor-pointer appearance-none rounded-full border-2 border-transparent bg-gray-200 ring-1 ring-transparent ring-offset-white transition-colors duration-200 ease-in-out before:inline-block before:h-6 before:w-6 before:translate-x-0 before:transform before:rounded-full before:bg-white before:shadow before:ring-0 before:transition before:duration-200 before:ease-in-out checked:bg-blue-600 checked:bg-none checked:before:translate-x-full checked:before:bg-blue-200 focus:outline-none dark:bg-gray-700 dark:before:bg-gray-400 dark:checked:bg-blue-600 dark:checked:before:bg-blue-200 dark:focus:ring-offset-gray-800"
+            />
+          </div>
 
-      {isLoading ? (
-        <button
-          disabled
-          className="mt-4 flex w-full items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 px-4 py-3 text-sm font-semibold text-white transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        >
-          <span
-            className="inline-block h-4 w-4 animate-spin rounded-full border-[3px] border-current border-t-transparent text-white"
-            role="status"
-            aria-label="loading"
-          ></span>
-          Creating
-        </button>
-      ) : (
-        <button
-          className="mt-4 w-full rounded-md border border-transparent bg-blue-500 px-4 py-3 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-zinc-500"
-          type="submit"
-          disabled={name.length < 3}
-        >
-          Create
-        </button>
-      )}
-    </Form>
+          {isLoading ? (
+            <button
+              disabled
+              className="mt-4 flex w-full items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 px-4 py-3 text-sm font-semibold text-white transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              <span
+                className="inline-block h-4 w-4 animate-spin rounded-full border-[3px] border-current border-t-transparent text-white"
+                role="status"
+                aria-label="loading"
+              ></span>
+              Creating
+            </button>
+          ) : (
+            <button
+              className="mt-4 w-full rounded-md border border-transparent bg-blue-500 px-4 py-3 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-zinc-500"
+              type="submit"
+              disabled={name.length < 3}
+            >
+              Create
+            </button>
+          )}
+        </Form>
+      </div>
+      <div className="mt-16 w-1/2 border-l border-gray-200 px-16 py-10">
+        <h1 className="text-lg font-bold text-slate-700">Usefull tips</h1>
+        <ul className="mt-4 flex list-inside list-disc flex-col  gap-3 text-sm text-slate-600">
+          <li>
+            <span className="font-semibold">A unique name is required </span>
+            as it will be the ID of the scene, as this is intended for personal
+            use all scenes are located at the root.{" "}
+          </li>
+          <li>
+            All scenes are
+            <span className="font-semibold"> private by default</span> and only
+            you can access them.
+          </li>
+          <li>
+            By the time being, you can't change the name once it's created.
+            <span className="font-semibold"> (coming soon ™)</span>
+          </li>
+          <li>
+            Your scene will be saved locally and in the cloud. It will sync
+            automatically every few seconds if it detects new changes.
+          </li>
+          <li>
+            If you have any config or feature you would like to see, let me know
+            at{" "}
+            <a
+              className="font-semibold"
+              href="https://puntogris.com"
+              target="_blank"
+            >
+              @puntogris.
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
